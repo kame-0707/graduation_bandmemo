046cb865cba46f09396a5c8d0810685a158e2048
kame-0707 <kamety20duffy@gmail.com>
2024-06-12 14:48:15 +0900

    フォームのカラム名を日本語化

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Myapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Asia/Tokyo'
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb, yml}')]
   end
 end
