3bedd804db85e193e08d01ead2fd1f6921474ca0
kame-0707 <kamety20duffy@gmail.com>
2024-06-12 22:35:57 +0900

    本番環境リダイレクト先をgooglecloudで設定

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -110,7 +110,7 @@ Rails.application.config.sorcery.configure do |config|
   config.google.key = Rails.application.credentials.dig(:google, :google_client_id)
   config.google.secret = Rails.application.credentials.dig(:google, :google_client_secret)
   #API設定で承認済みのリダイレクトURIとして登録したurlを設定
-  config.google.callback_url = Settings.sorcery(:google_callback_url)
+  config.google.callback_url = Settings.sorcery{:google_callback_url}
   #外部サービスから取得したユーザー情報をUserモデルの指定した属性にマッピング
   config.google.user_info_mapping = {email: "email", name: "name"}
 
