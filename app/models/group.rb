8254de5a0056a649db5fc632adaa2333d137f89f
kame-0707 <kamety20duffy@gmail.com>
2024-07-04 00:25:54 +0900

    トップページとヘッダーに、グループ機能の使い方説明を追記

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
-  has_many :summaries
-  has_many :voices
+  has_many :summaries, dependent: :destroy
+  has_many :voices, dependent: :destroy
   has_many :group_users, dependent: :destroy
   has_many :permits, dependent: :destroy
   has_many :users, through: :group_users
