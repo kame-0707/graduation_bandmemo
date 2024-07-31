f9791c73565b6a498c8fb7089297a90919b0751c
kame-0707 <kamety20duffy@gmail.com>
2024-07-31 23:19:45 +0900

    fix: パスワードリセットメールのタイトル文言修正

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,6 @@ class UserMailer < ApplicationMailer
     @user = User.find user.id
     @url  = edit_password_reset_url(@user.reset_password_token)
     mail(:to => user.email,
-         :subject => "パスワードリセット")
+         :subject => "【bandmemo】パスワード再発行のご案内")
   end
 end
