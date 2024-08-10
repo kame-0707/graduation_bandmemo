71e96fe8c73c12aadedcc79ad147c7e86131ac29
kame-0707 <kamety20duffy@gmail.com>
2024-08-10 20:29:20 +0900

    fix: ルーティーンの修正

--- config/routes.rb
+++ config/routes.rb
@@ -43,7 +43,6 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
 
-  get 'oauths/oauth'
   post 'oauth/callback' => 'oauths#callback'
   get 'oauth/callback' => 'oauths#callback'
   get 'oauth/:provider' => 'oauths#oauth', :as => :auth_at_provider
