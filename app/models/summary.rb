6133a8cbd2af8d9b82e1e2035116c21a0213b37f
kame-0707 <kamety20duffy@gmail.com>
2024-09-15 23:03:58 +0900

    fix: summary.rbからsummaryモデルのバリデーションを削除(そのまま保存機能を有効にするため)

--- app/models/summary.rb
+++ app/models/summary.rb
@@ -8,5 +8,4 @@ class Summary < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 255 }
   validates :content, presence: true, length: { maximum: 65_535 }
-  validates :summary, presence: true, length: { maximum: 65_535 }
 end
